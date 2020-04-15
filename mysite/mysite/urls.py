a29d0dabb8962c33d00902f2576ed76d669281ce
jack2012aa <you@example.com>
2020-04-15 21:10:02 +0800

    Add user login.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -32,3 +32,8 @@ urlpatterns += [
     path('',RedirectView.as_view(url='/catalog/')),
 ]
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
