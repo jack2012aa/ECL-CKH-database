35947ef44e7aabfbdc97f6913db6ee9aa32885d3
jack2012aa <you@example.com>
2020-05-21 15:43:19 +0800

    simplify the code, add pig, data history

--- mysite/catalog/models.py
+++ mysite/catalog/models.py
@@ -4,7 +4,6 @@ from django.urls import reverse #Used to generate URLs by reversing the URL patt
 # Create your models here.
 
 class Pig(models.Model):
-    """Model representing an author."""
     pig_id = models.CharField(max_length=8,primary_key=True, help_text='Input birth year(XX) + ear tag')
     birth = models.DateField(null=True, blank=True)
     gender = models.CharField(max_length=100)
@@ -26,37 +25,23 @@ class Pig(models.Model):
         return ', '.join(genre.name for genre in self.genre.all()[:3])
 
 class Pig_history(models.Model):
-    """Model representing an author."""
     pig_id = models.CharField(null = True, max_length=8, help_text='Input birth year(XX) + ear tag')
     birth = models.DateField(null=True, blank=True)
     gender = models.CharField(null=True, max_length=100)
     dad_id = models.CharField(null=True, max_length=100)
     mom_id = models.CharField(null=True, max_length=100)
     breed = models.CharField(max_length=10, help_text='Pig\'s breed', blank=True, null=True)
-    modified_date = models.DateTimeField()
-
-    def get_absolute_url(self):
-        """Returns the url to access a particular author instance."""
-        return reverse('pig-detail', args=[str(self.pig_id)])
-
+    modified_date = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         """String for representing the Model object."""
         # return f'{self.pig_id}'
         return self.pig_id
-    
-    def display_genre(self):
-        """Create a string for the Genre. This is required to display genre in Admin."""
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
 
 class Data(models.Model):
     """Model representing measuring datas."""
     data_id = models.AutoField(primary_key=True)
     pig_id = models.ForeignKey('Pig', on_delete=models.SET_NULL, null=True)
-    
-    # Foreign Key used because a data can only belong to one pig, but a pig can have multiple datas
-    # Pig as a string rather than object because it hasn't been declared yet in the file.
-    
-
     weight = models.FloatField(help_text='Input weight in kg.')
     length = models.FloatField(help_text='Input body length in cm.')
     height = models.FloatField(help_text='Input height in cm.')
@@ -77,11 +62,26 @@ class Data(models.Model):
         null=True,
         )   
 
-    
-    # ManyToManyField used because genre can contain many books. pigs can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    
     def __str__(self):
         """String for representing the Model object."""
         return str(self.data_id)
     
+class Data_history(models.Model):
+    """Model representing measuring datas."""
+    data_id = models.IntegerField(null=True)
+    pig_id = models.ForeignKey('Pig', on_delete=models.SET_NULL, null=True)
+    weight = models.FloatField(null=True)
+    length = models.FloatField(null=True)
+    height = models.FloatField(null=True)
+    front_width = models.FloatField(null=True)
+    back_width = models.FloatField(null=True)
+    depth = models.FloatField(null=True) 
+    chest = models.FloatField(null=True)   
+    front_cannon_circumference = models.FloatField(null=True)
+    back_cannon_circumference = models.FloatField(null=True)
+    date = models.DateField(null=True)   
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return str(self.data_id)
--- mysite/catalog/views.py
+++ mysite/catalog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib import messages
-
 from django.http import HttpResponse
+from django.utils.decorators import classonlymethod
+from django.views import generic
 import csv, io
 import datetime
+from .models import Data, Pig, Pig_history, Data_history
 # Create your views here.
 
-from .models import Data, Pig, Pig_history
+
 
 def is_valid_queryparam(param):
     return (param != '' and param is not None)
@@ -14,12 +16,74 @@ def is_valid_queryparam(param):
 def check_change(object_history_data, column, fields):
     i = 0
     for field in fields:
-        if getattr(object_history_data, field) != column[i]:
-            print(getattr(object_history_data, field), column[i])
+        if str(getattr(object_history_data, field)) != str(column[i]):
             return True
         i += 1
     return False
 
+def listfilter(request, model, fields):
+    global qs
+    qs = model.objects.all()
+    for field in fields:
+        param = request.GET.get(field)
+        if is_valid_queryparam(param):
+            qs = qs.filter(**{field:param})
+        else:
+            param_up_down = ['_min', '_max']
+            field_up_down = ['__gte', '__lt']
+            for i in range(2):
+                param_range = str(field) + str(param_up_down[i])
+                field_range = str(field) + str(field_up_down[i])
+                param = request.GET.get(param_range)
+                print('====', field_range, param, param_range, '====')
+                if is_valid_queryparam(param):
+                    qs = qs.filter(**{field_range:param})
+    return qs
+    
+def foreignfilter(request, foreignkey, foreignkeyfields, qs):
+    for foreignkeyfield in foreignkeyfields:
+        field = str(foreignkey) + '__' + str(foreignkeyfield)
+        param = request.GET.get(foreignkeyfield)
+        if is_valid_queryparam(param):
+            qs = qs.filter(**{field:param})
+        else:
+            param_up_down = ['_min', '_max']
+            field_up_down = ['__gte', '__lt']
+            for i in range(2):
+                param_range = str(foreignkeyfield) + str(param_up_down[i])
+                field_range = str(field) + str(field_up_down[i])
+                param = request.GET.get(param_range)
+                if is_valid_queryparam(param):
+                    qs = qs.filter(**{field_range:param})
+    return qs
+    
+def upload_csv(request, model, fields, model_history):
+    csv_file = request.FILES['files']
+    #Check if it is a csv file
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'THIS IS NOT A CSV FILE')
+    data_set = csv_file.read().decode('UTF_8')
+    io_string = io.StringIO(data_set)
+    next(io_string)
+    #Upload to the database
+    for column in csv.reader(io_string, delimiter=',', quotechar='\\'):
+        try:
+            object_history_data = model.objects.get(**{fields[0]:column[0]})
+            object_history = model_history()
+            if check_change(object_history_data, column, fields):
+                for field in fields:
+                    field_history = getattr(object_history_data, field)
+                    setattr(object_history, field, field_history)
+                    object_history.save()
+        except:
+            pass
+        
+        defaults = {}
+        for i in range(len(column) - 1):
+            defaults[fields[i + 1]] = column[i + 1]
+        created = model.objects.update_or_create({fields[0]:column[0]}, defaults=defaults) 
+    io_string.close()
+        
 def index(request):
     """View function for home page of site."""
 
@@ -38,238 +102,40 @@ def index(request):
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-from django.views import generic
-
-def DataListFilter(request):
-    #Generate filter from request
-    '''Refer to https://github.com/justdjango/djfilter'''
-    #global variable for export
-    global qs
-    qs = Data.objects.all()
-    data_id = request.GET.get('data_id')
-    pig_id = request.GET.get('pig_id')
-    pig_gender = request.GET.get('pig_gender')
-    pig_breed = request.GET.get('pig_breed')
-    pig_birth_min = request.GET.get('pig_birth_min')
-    pig_birth_max = request.GET.get('pig_birth_max')
-    data_weight_min = request.GET.get('data_weight_min')
-    data_weight_max = request.GET.get('data_weight_max')
-    data_length_min = request.GET.get('data_length_min')
-    data_length_max = request.GET.get('data_length_max')
-    data_height_min = request.GET.get('data_height_min')
-    data_height_max = request.GET.get('data_height_max')
-    data_front_width_min = request.GET.get('data_front_width_min')
-    data_front_width_max = request.GET.get('data_front_width_max')
-    data_back_width_min = request.GET.get('data_back_width_min')
-    data_back_width_max = request.GET.get('data_back_width_max')
-    data_depth_min = request.GET.get('data_depth_min')
-    data_depth_max = request.GET.get('data_depth_max')
-    data_chest_min = request.GET.get('data_chest_min')
-    data_chest_max = request.GET.get('data_chest_max')
-    data_front_cir_min = request.GET.get('data_front_cir_min')
-    data_front_cir_max = request.GET.get('data_front_cir_max')
-    data_back_cir_min = request.GET.get('data_back_cir_min')
-    data_back_cir_max = request.GET.get('data_back_cir_max')
-    data_date_min = request.GET.get('data_date_min')
-    data_date_max = request.GET.get('data_date_max')
-        
-    if is_valid_queryparam(data_id):
-        qs = qs.filter(data_id=data_id)
-
-    if is_valid_queryparam(pig_id):
-        qs = qs.filter(pig_id__pig_id=pig_id) 
-            
-    if is_valid_queryparam(pig_gender):
-        qs = qs.filter(pig_id__gender=pig_gender)
-        
-    if is_valid_queryparam(pig_breed):
-        qs = qs.filter(pig_id__breed=pig_breed)
-        
-    if is_valid_queryparam(pig_birth_min):
-        qs = qs.filter(pig_id__birth__gte=pig_birth_min)
-
-    if is_valid_queryparam(pig_birth_max):
-        qs = qs.filter(pig_id__birth__lt=pig_birth_max)
-        
-    if is_valid_queryparam(data_weight_min):
-        qs = qs.filter(weight__gte=data_weight_min)   
-
-    if is_valid_queryparam(data_weight_max):
-        qs = qs.filter(weight__lt=data_weight_max)
-        
-    if is_valid_queryparam(data_length_min):
-        qs = qs.filter(length__gte=data_length_min)
-
-    if is_valid_queryparam(data_length_max):
-        qs = qs.filter(length__lt=data_length_max)
-        
-    if is_valid_queryparam(data_height_min):
-        qs = qs.filter(height__gte=data_height_min)
-
-    if is_valid_queryparam(data_height_max):
-        qs = qs.filter(height__lt=data_height_max)
-        
-    if is_valid_queryparam(data_front_width_min):
-        qs = qs.filter(front_width__gte=data_front_width_min)  
-
-    if is_valid_queryparam(data_front_width_max):
-        qs = qs.filter(front_width__lt=data_front_width_max)
-        
-    if is_valid_queryparam(data_back_width_min):
-        qs = qs.filter(back_width__gte=data_back_width_min)
-
-    if is_valid_queryparam(data_back_width_max):
-        qs = qs.filter(back_width__lt=data_back_width_max)
-        
-    if is_valid_queryparam(data_depth_min):
-        qs = qs.filter(depth__gte=data_depth_min)
-
-    if is_valid_queryparam(data_depth_max):
-        qs = qs.filter(depth_lt=data_depth_max)
-        
-    if is_valid_queryparam(data_chest_min):
-        qs = qs.filter(chest__gte=data_chest_min)  
-            
-    if is_valid_queryparam(data_chest_max):
-        qs = qs.filter(chest__lt=data_chest_max)
-        
-    if is_valid_queryparam(data_front_cir_min):
-        qs = qs.filter(front_cannon_circumference__gte=data_front_cir_min)
-
-    if is_valid_queryparam(data_front_cir_max):
-        qs = qs.filter(front_cannon_circumference__lt=data_front_cir_max)
-        
-    if is_valid_queryparam(data_back_cir_min):
-        qs = qs.filter(back_cannon_circumference__gte=data_back_cir_min)
-
-    if is_valid_queryparam(data_back_cir_max):
-        qs = qs.filter(data_back_cir_max=data_back_cir_max)
-        
-    if is_valid_queryparam(data_date_min):
-        qs = qs.filter(date__gte=data_date_min)  
-
-    if is_valid_queryparam(data_date_max):
-        qs = qs.filter(date__lt=data_date_max)
-        
-    return qs
-
-
-def PigListFilter(request):
-    #Generate filter from request
-    '''Refer to https://github.com/justdjango/djfilter'''
-    #global variable for export
-    global qs
-    qs = Pig.objects.all()
-    pig_id = request.GET.get('pig_id')
-    pig_gender = request.GET.get('pig_gender')
-    pig_breed = request.GET.get('pig_breed')
-    pig_dad_id = request.GET.get('pig_dad_id')
-    pig_mom_id = request.GET.get('pig_mom_id')
-    pig_birth_from = request.GET.get('pig_birth_from')
-    pig_birth_to = request.GET.get('pig_birth_to')
-    
-
-    if is_valid_queryparam(pig_id):
-        qs = qs.filter(pig_id=pig_id) 
-            
-    if is_valid_queryparam(pig_gender):
-        qs = qs.filter(gender=pig_gender)
-        
-    if is_valid_queryparam(pig_breed):
-        qs = qs.filter(breed=pig_breed)
-
-    if is_valid_queryparam(pig_dad_id):
-        qs = qs.filter(dad_id=pig_dad_id) 
-
-    if is_valid_queryparam(pig_mom_id):
-        qs = qs.filter(mom_id=pig_mom_id)
-
-    if is_valid_queryparam(pig_birth_from):
-        qs = qs.filter(birth__gte=pig_birth_from)
-
-    if is_valid_queryparam(pig_birth_to):
-        qs = qs.filter(birth__lt=pig_birth_to)
-        
-    return qs
-
-
-def DataListView(request):
-    if request.method == 'GET':
-        context = {'data_list': DataListFilter(request)}
-        return render(request, 'data_list.html', context)
-    
-    if request.method =='POST':
-        #POST request
-        ''''Refer to https://medium.com/@simathapa111/how-to-upload-a-csv-file-in-django-3a0d6295f624'''
-        #Get uploaded file
-        csv_file = request.FILES['files']
-        #Check if it is a csv file
-        if not csv_file.name.endswith('.csv'):
-            messages.error(request, 'THIS IS NOT A CSV FILE')
-        data_set = csv_file.read().decode('UTF_8')
-        io_string = io.StringIO(data_set)
-        next(io_string)
-        #Upload to the database
-        for column in csv.reader(io_string, delimiter=',', quotechar='\\'):
-            created = Data.objects.update_or_create(
-                pig_id=Pig.objects.filter(pig_id=column[0])[0],
-                weight=column[1],
-                length=column[2],
-                height=column[3],
-                front_width=column[4],
-                back_width=column[5],
-                depth=column[6],
-                chest=column[7],
-                front_cannon_circumference=column[8],
-                back_cannon_circumference=column[9],
-                date=column[10],
-                )
-        io_string.close()
-        context = {'data_list': DataListFilter(request)}
-        return render(request, 'data_list.html', context=context)
-
-
 def PigListView(request):
-    #GET request
+    model = Pig
+    fields = ['pig_id', 'birth', 'gender', 'dad_id', 'mom_id', 'breed']
+    model_history = Pig_history
+    template = 'pig_list.html'
     if request.method == 'GET':
-        context = {'pig_list': PigListFilter(request)}
-        return render(request, 'pig_list.html', context=context)
+        qs = listfilter(request,model,fields)
+        context = {'list':qs}
+        return render(request,template,context)
+    elif request.method == 'POST':
+        upload_csv(request, model, fields, model_history)
+        qs = listfilter(request,model,fields)
+        context = {'list':qs}
+        return render(request,template,context)
     
-    #POST request
-    ''''Refer to https://medium.com/@simathapa111/how-to-upload-a-csv-file-in-django-3a0d6295f624'''
-    #Get uploaded file
-    csv_file = request.FILES['files']
-    #Check if it is a csv file
-    if not csv_file.name.endswith('.csv'):
-        messages.error(request, 'THIS IS NOT A CSV FILE')
-    data_set = csv_file.read().decode('UTF_8')
-    io_string = io.StringIO(data_set)
-    next(io_string)
-    #Upload to the database
-    fields = ['pig_id','birth','gender','dad_id', 'mom_id', 'breed']
-    for column in csv.reader(io_string, delimiter=',', quotechar='\\'):
-        try:
-            object_history_data = Pig.objects.get(pig_id=column[0])
-            object_history = Pig_history()
-            if check_change(object_history_data, column, fields):
-                for field in fields:
-                    field_history = getattr(object_history_data, field)
-                    setattr(object_history, field, field_history)
-                object_history.modified_date = datetime.datetime.now()
-                object_history.save()
-        except:
-            pass
-                
-        created = Pig.objects.update_or_create(
-            pig_id=column[0],
-            defaults={'birth':column[1], 'gender':column[2], 'dad_id':column[3], 
-                      'mom_id':column[4], 'breed':column[5],
-                      }
-            )
-    io_string.close()
-    context = {'pig_list': PigListFilter(request)}
-    return render(request, 'pig_list.html', context=context)
-
+def DataListView(request):
+    model = Data
+    fields = ['data_id', 'pig_id', 'weight', 'length', 'height', 'front_width', 'back_width', 'depth', 
+              'chest', 'front_cannon_circumference', 'back_cannon_circumference', 'date']
+    model_history = Data_history
+    template = 'data_list.html'
+    foreignkey = 'pig_id'
+    foreignkeyfields = ['pig_id', 'gender', 'breed', 'birth']
+    if request.method == 'GET':
+        qs = listfilter(request,model,fields)
+        qs = foreignfilter(request, foreignkey, foreignkeyfields, qs)
+        context = {'list':qs}
+        return render(request,template,context)
+    elif request.method == 'POST':
+        upload_csv(request, model, fields, model_history)
+        qs = listfilter(request,model,fields)
+        qs = foreignfilter(request, foreignkey, foreignkeyfields, qs)
+        context = {'list':qs}
+        return render(request,template,context)
 
 def export_piglist(request):
     '''Refer to https://docs.djangoproject.com/en/3.0/howto/outputting-csv/'''
@@ -296,10 +162,10 @@ def export_datalist(request):
     
     #Create a csv file
     writer = csv.writer(response)
-    writer.writerow(['Pig id', '重量', '體長', '體高', '前寬', '後寬', '體深', '胸深' ,'前管圍', 
+    writer.writerow(['Data id', 'Pig id', '重量', '體長', '體高', '前寬', '後寬', '體深', '胸深' ,'前管圍', 
                      '後管圍' ,'拍攝日期'])
     for data in qs:
-        writer.writerow([data.pig_id, data.weight, data.length, data.height, data.front_width, 
+        writer.writerow([data.data_id, data.pig_id, data.weight, data.length, data.height, data.front_width, 
                          data.back_width, data.depth, data.chest, data.front_cannon_circumference,
                          data.back_cannon_circumference, data.date])
     return response
@@ -307,42 +173,16 @@ def export_datalist(request):
 
 class DataDetailView(generic.DetailView):
     model = Data
-    ''' if writen in function view
-    def data_detail_view(request, primary_key):
-        try:
-            data = Data.objects.get(pk=primary_key)
-        except Data.DoesNotExist:
-            raise Http404('Data does not exist')
-
-        from django.shortcuts import get_object_or_404
-        data = get_object_or_404(Data, pk=primary_key)
-        
-        return render(request, 'catalog/data_detail.html', context={'data': data})
-        '''
-
 
 class PigDetailView(generic.DetailView):
     model = Pig
-    
-    #  if writen in function view
-    def pig_detail_view(request, primary_key):
-        try:
-            pig = pig.objects.get(pk=primary_key)
-        except Pig.DoesNotExist:
-            raise Http404('Pig does not exist')
-
-        from django.shortcuts import get_object_or_404
-        pig = get_object_or_404(Pig, pk=primary_key)
-        
-        return render(request, 'catalog/pig_detail.html', context={'pig': pig})
-    
 
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
 class PigCreate(CreateView):
     model = Pig
     fields = '__all__'
-
 
 class Update_with_historyView(UpdateView):
     history_model = None
@@ -353,7 +193,6 @@ class Update_with_historyView(UpdateView):
         for field in self.fields:
             field_history = getattr(object_history_data, field)
             setattr(object_history, field, field_history)
-        object_history.modified_date = datetime.datetime.now()
         object_history.save()
         return super().post(request, *args, **kwargs)
 
